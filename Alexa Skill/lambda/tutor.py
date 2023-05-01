c80071ee435b1795adedc8379a31b5435533c554
wherrmat <132291180+wherrmat@users.noreply.github.com>
2023-05-01 20:12:24 +0200

    Update tutor.py

--- Alexa Skill/lambda/tutor.py
+++ Alexa Skill/lambda/tutor.py
@@ -1,18 +1,16 @@
 import openai
 
-class tutor():
-    # Indica el API Key
-    openai.api_key = ""
-    
-    # Uso de ChapGPT en Python
-    model_engine = "text-davinci-002"
-    
-    def ask_gpt(question):
-        prompt = question
-        completion = openai.Completion.create(engine=model_engine,
+# Indica el API Key
+openai.api_key = "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"
+# Uso de ChapGPT en Python
+model_engine = "text-davinci-002"
+
+def ask_gpt(question):
+    prompt = question
+    completion = openai.Completion.create(engine=model_engine,
                                           prompt=prompt,
                                           max_tokens=1024,
                                           n=1,
                                           stop=None,
                                           temperature=0.7)
-        return completion.choices
+    return completion.choices
